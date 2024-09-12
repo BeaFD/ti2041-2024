528b7917a5ace0a73431630b00c1d003177343bc
BeaFD <164698472+BeaFD@users.noreply.github.com>
2024-09-12 15:39:53 -0300

    Ev. Formativa 1

--- evaluaciones/sumativa1/sumativa1/urls.py
+++ evaluaciones/sumativa1/sumativa1/urls.py
@@ -6,4 +6,5 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('productos/', include('productos.urls')),
+    path('', RedirectView.as_view(url='/productos/consulta', permanent=True))
 ]
